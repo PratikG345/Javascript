8fbe9c93e7f44cc7d259b41ffaba542d39aa2b08
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-07-20 08:51:24 +0000

    'message'

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+// alert (3+3) // we are using nodejs not browser
+
+let name = "pratik"
+let ag = 21
+let isLoggedIn = true
+// number >= 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null >= standalone value // null can be defined 
+// undefined => // undefined means the value is not assigned
+// symbol = uniqueness
+
+// object
+
+console.log(typeof ag)
+console.log(typeof null) // datatype of null is "Object"
+console.log(typeof undefined) // datatype of undefined is "Undefined"
---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accountId =  1444553
+let accountEmail = "pratik678@fmail.com"
+var accountPassword = "12345" 
+/*Prefer not to use var
+because of issue in block scope and functional scope
+*/
+accountCity = "Jaipur"
+let accountState = false;
+
+// accountId = 2 //not allowed
+accountEmail = "hc123@fmail.com"
+accountPassword = "67890"
+accountCity = "Mumbai"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountId);
+// console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
