d59b51afce16b68a524df4ac239b4e7d661737ef
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-07-20 09:59:26 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,16 @@
+"use strict"
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+
+// === : strict check, it not only checks the value but also the datatype.
+console.log("2" == 2) // true
+console.log("2" === 2) // false 
+// no converion will take place in strict check
