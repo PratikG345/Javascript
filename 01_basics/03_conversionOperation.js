de143e702672b19d3ca588fee49d3b819174728b
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-07-20 09:48:45 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,9 +33,36 @@ undefined => false
 
 let someNumber = undefined
 let srtingNumber = String(someNumber)
-console.log(srtingNumber);
-console.log(typeof(srtingNumber));
+// console.log(srtingNumber);
+// console.log(typeof(srtingNumber));
 
 /*
 33 => 33
 */
+
+// ************** operations ***************
+
+let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); //4
+// console.log(2-2); // 0
+// console.log(2*2);// 4
+// console.log(2**3); // means 2 to the power of 3 // 8
+// console.log(2/2); // 1
+// console.log(2%2); // Remainder // 0  
+
+let str1 = "Pratik"
+let str2 = " Gaikwad"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2) // 12
+console.log("1" + "2") // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log("1" + (2 + 2)) // here first 2+2 becomes 4 then joints with 1
+console.log(1 + 2 + "2") // here 1+2 becomes 3 and joints with 2,  // 32
+
+
