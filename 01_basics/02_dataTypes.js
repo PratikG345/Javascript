da29af47715934af2becd3b3fdc1567a318b30f1
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-07-20 10:02:00 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,8 +1,9 @@
 "use strict"; // treat all JS code as newer version
 // alert (3+3) // we are using nodejs not browser
+//datatypes
 
 let name = "pratik"
-let ag = 21
+let age = 21
 let isLoggedIn = true
 // number >= 2 to power 53
 // bigint
@@ -14,6 +15,6 @@ let isLoggedIn = true
 
 // object
 
-console.log(typeof ag)
+console.log(typeof age)
 console.log(typeof null) // datatype of null is "Object"
 console.log(typeof undefined) // datatype of undefined is "Undefined"
