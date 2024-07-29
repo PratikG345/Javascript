5c9fc5bd66411bdf01d2464f480eaecc2d07a074
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-07-29 10:15:16 +0000

    Date and TIme

---
+++ 01_basics/08_datesInJs.js
@@ -0,0 +1,26 @@
+let mydate = new Date
+// console.log(mydate)
+// console.log(typeof mydate) // object
+// console.log(mydate.getTime())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toTimeString())
+// console.log(mydate.getFullYear())
+
+//let createddate = new Date(2023 , 0 , 23, 15 , 30, 44)
+// let createddate = new Date("07/29/2024 15:31:44")
+
+let timestamp = Date.now()
+// console.log(timestamp) // shows date in milliseconds
+// console.log(Math.floor(Date.now()/1000)) // shows date in seconds
+
+let newDate = new Date
+console.log(newDate)
+console.log(newDate.getTime())
+console.log(newDate.getFullYear())
+console.log(newDate.getDay())
+console.log(newDate.getMonth()+1)
+console.log(newDate.getHours())
+
+
