a27c5cc75ff1da129a577ebeaa51c1e017f9482a
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-08-23 08:44:51 +0000

    scope level and mini hoisting

---
+++ 03_basics/03_scopelevel.js
@@ -0,0 +1,22 @@
+if(true){
+    const username = "Pratik"
+    if(true){
+        const website = " YouTube"
+        console.log(username + website);
+    }
+    console.log(username);
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+console.log(addtwo(5))
+const addtwo = function(num){
+    return num+2
+}
